add6b6b555906385665c76df48f59e572c20302b
Craig Banach <craigsb93@gmail.com>
2026-04-24 23:18:05 +0100

    Unsafely use the first arg as the file path

--- parkrun-analyser.rs
+++ parkrun-analyser.rs
@@ -1,10 +1,15 @@
+use std::env;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::io::BufReader;
 
 fn main() -> io::Result<()> {
-    let f = File::open("./samples/parkrun-results.csv")?;
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    let f = File::open(file_path)?;
     let reader = BufReader::new(f);
 
     for line in reader.lines() {
