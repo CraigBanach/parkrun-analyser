714f54ee609d06618ec35b7a0e81dac58c05eafe
Craig Banach <craigsb93@gmail.com>
2026-04-24 23:06:03 +0100

    Read file to console

--- parkrun-analyser.rs
+++ parkrun-analyser.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello World!");
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn main() -> io::Result<()> {
+    let f = File::open("./samples/parkrun-results.csv")?;
+    let reader = BufReader::new(f);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+    Ok(())
 }
