ab8183059a46aec73e6dafb7ac8daf986ec97be7
Craig Banach <craigsb93@gmail.com>
2026-05-04 23:12:22 +0100

    Use RunRecord, populated with dummy values when reading from CSV

--- src/app.rs
+++ src/app.rs
@@ -6,7 +6,7 @@ use crate::io::{RowOutcome, row_outcomes};
 pub(crate) fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     for outcome in row_outcomes(config.file_path())? {
         match outcome {
-            RowOutcome::Record(line) => println!("{}", line),
+            RowOutcome::Record(line) => println!("{:?}", line),
             RowOutcome::Warning(msg) => println!("warning: {}", msg),
         }
     }
--- src/io.rs
+++ src/io.rs
@@ -1,7 +1,9 @@
+use chrono::NaiveDate;
 use std::error::Error;
 use std::io::BufReader;
 use std::io::Lines;
 use std::result::Result;
+use std::time::Duration;
 use std::{fs::File, io::BufRead};
 
 fn file_lines(file_path: &str) -> Result<Lines<BufReader<File>>, Box<dyn Error>> {
@@ -10,8 +12,19 @@ fn file_lines(file_path: &str) -> Result<Lines<BufReader<File>>, Box<dyn Error>>
     Ok(reader.lines())
 }
 
+#[derive(Debug)] // TODO: implement Display
+pub(crate) struct RunRecord {
+    date: NaiveDate,
+    event_name: String,
+    run_number: u16,
+    finish_position: u32,
+    time: Duration,
+    age_grade: u32,
+    personal_best: bool,
+}
+
 pub(crate) enum RowOutcome {
-    Record(String),
+    Record(RunRecord),
     Warning(String),
 }
 
@@ -19,7 +32,15 @@ pub(crate) fn row_outcomes(
     file_path: &str,
 ) -> Result<impl Iterator<Item = RowOutcome>, Box<dyn Error>> {
     Ok(file_lines(file_path)?.map(|line| match line {
-        Ok(line) => RowOutcome::Record(line),
+        Ok(_line) => RowOutcome::Record(RunRecord {
+            date: NaiveDate::MIN,
+            event_name: "Lochore Meadows".to_string(),
+            run_number: 1,
+            finish_position: 1,
+            time: Duration::MAX,
+            age_grade: 10000,
+            personal_best: true,
+        }),
         Err(e) => RowOutcome::Warning(e.to_string()),
     }))
 }
